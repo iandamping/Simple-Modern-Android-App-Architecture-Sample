491dd322f6fc667ff0e001b33bf67dbca6588123
ian damping <ian.damping@spesolution.com>
2022-05-24 10:54:13 +0700

    classic architecture for android

--- app/src/main/java/com/example/modernarchitecturesample/MainActivity.kt
+++ app/src/main/java/com/example/modernarchitecturesample/MainActivity.kt
@@ -2,14 +2,19 @@ package com.example.modernarchitecturesample
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.example.modernarchitecturesample.adapter.MovieAdapter
+import coil.load
+import com.example.modernarchitecturesample.databinding.ItemMovieBinding
 import com.example.modernarchitecturesample.model.MovieResponse
 import com.example.modernarchitecturesample.network.ApiInterface
 import com.example.modernarchitecturesample.network.MainResponse
@@ -128,6 +133,67 @@ class MainActivity : AppCompatActivity(), MovieAdapter.MovieAdapterListener {
         startActivity(intent)
     }
 
+}
+
+class MovieAdapter(
+    private val listener: MovieAdapterListener
+) :
+    ListAdapter<MovieResponse, MovieAdapter.MovieViewHolder>(listMoviedapterCallback) {
+
+    interface MovieAdapterListener {
+        fun onClicked(data: MovieResponse)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        return MovieViewHolder(
+            ItemMovieBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val data = getItem(position)
+        holder.bind(data)
+        holder.itemView.setOnClickListener {
+            listener.onClicked(data)
+        }
+    }
+
+    inner class MovieViewHolder(
+        private val binding: ItemMovieBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        private val imageFormatter = "https://image.tmdb.org/t/p/w500"
+
+        fun bind(data: MovieResponse) {
+            with(binding) {
+                ivMovieImage.load(imageFormatter + data.backdrop_path) {
+                    placeholder(R.drawable.ic_placeholder)
+                    error(R.drawable.ic_error)
+                }
+                tvMovieName.text = data.title
+                tvMovieReleaseDate.text = data.overview
+            }
+        }
+    }
+
+    companion object {
+        val listMoviedapterCallback = object : DiffUtil.ItemCallback<MovieResponse>() {
+            override fun areItemsTheSame(oldItem: MovieResponse, newItem: MovieResponse): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(
+                oldItem: MovieResponse,
+                newItem: MovieResponse
+            ): Boolean {
+                return oldItem.poster_path == newItem.poster_path
+            }
+        }
+    }
 }
 
 
