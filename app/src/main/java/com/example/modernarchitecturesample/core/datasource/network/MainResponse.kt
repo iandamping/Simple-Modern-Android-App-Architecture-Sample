04253285aee613dae6aea013c6bb738470c20b9c
ian damping <ian.damping@spesolution.com>
2022-05-24 15:09:39 +0700

    classic architecture for android

--- app/src/main/java/com/example/modernarchitecturesample/core/datasource/network/MainResponse.kt
+++ app/src/main/java/com/example/modernarchitecturesample/core/datasource/network/MainResponse.kt
@@ -1,8 +1,9 @@
 package com.example.modernarchitecturesample.core.datasource.network
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
-
+@JsonClass(generateAdapter = true)
 data class MainResponse<T>(
     @Json(name = "results") val results: List<T>
 )
